2787b4f90f4205b57071dbb85c1c0d68ea9286db
jasonincanada <nirgle@gmail.com>
2018-12-27 03:09:43 -0500

    add cover(i), hide(i), uncover(i), unhide(i)

--- app/Main.hs
+++ app/Main.hs
@@ -9,5 +9,11 @@ main :: IO ()
 main = do
   contents <- readFile file
   let links = run parseLinksFile contents
-  let table = tableFromLinks links
+
+  let table     = tableFromLinks links
+  let covered   = cover 1 table
+  let uncovered = uncover 1 covered
+
+  -- These should be identical
   putStrLn $ show table
+  putStrLn $ show uncovered
--- src/DancingLinks.hs
+++ src/DancingLinks.hs
@@ -11,13 +11,15 @@
    So it is not too useful yet but this is the initial version that loads in and understands
    the format of a *.links file.
 
+   This revision implements cover(i), hide(i), uncover(i), unhide(i) [operations (12)-(15)]
+
 
    References:
 
    Donald Knuth introducing Dancing Links
      https://www.youtube.com/watch?v=_cR9zDlvP88
 
-   The Art of Computer Programming, Vol 4, Pre-fascicle 5C
+   [Knuth] The Art of Computer Programming, Vol 4, Pre-fascicle 5C
      https://www-cs-faculty.stanford.edu/~knuth/
 -}
 
@@ -25,7 +27,9 @@
 
 module DancingLinks
     ( parseLinksFile,
-      tableFromLinks
+      tableFromLinks,
+      cover,
+      uncover
     ) where
 
 import           Control.Applicative (some, many)
@@ -34,6 +38,7 @@ import qualified Data.IntMap as IntMap
 import           Data.Char (isAlphaNum)
 import           Data.List (sort)
 import           NanoParsec hiding (item, option)
+
 
 ----------------
 -- Types -------
@@ -179,4 +184,102 @@ indicesOf alphabet as = let
                                    Nothing -> 0 -- really an error, we don't recognize this item
                                    Just x  -> x
                         in map f as
+
+
+------------------
+-- Operations ----
+------------------
+
+-- This is (12) from [Knuth].  Cover an item by unlinking its top node and all other items in
+-- any option containing that item
+cover :: NodeIndex -> DLTable -> DLTable
+cover i (DLTable ns ss nodes) = DLTable ns ss nodes'
+  where
+    nodes' = go (node i ^. dlink) nodes
+    node n = nodes IntMap.! n
+
+    go p ns | p == i    = let l = node i ^. llink
+                              r = node i ^. rlink
+                          in  unlink l r ns
+            | otherwise = go (node p ^. dlink) (hide p ns)
+
+    -- Unlink the top node by rewiring the nodes on its left/right to each other
+    unlink l r ns = let lnode = ns IntMap.! l
+                        rnode = ns IntMap.! r
+                    in  IntMap.insert l (lnode { _rlink = r }) $
+                        IntMap.insert r (rnode { _llink = l }) ns
+
+
+-- This is (13) from [Knuth]
+hide :: NodeIndex -> IntMap.IntMap Node -> IntMap.IntMap Node
+hide p nodes = nodes'
+  where
+    nodes' = go (p+1) nodes
+    node n = nodes IntMap.! n
+
+    go q ns | q == p    = ns
+            | otherwise = let x = node q ^. topLen
+                              u = node q ^. ulink
+                              d = node q ^. dlink
+                          in
+                              -- Is q a spacer node
+                              if x <= 0
+                              then go u     ns
+                              else go (q+1) (unlink x u d ns)
+
+    -- Unlink a node by rewiring the nodes above/below it to each other
+    unlink x u d ns = let unode = ns IntMap.! u
+                          dnode = ns IntMap.! d
+                          tnode = ns IntMap.! x
+                      in
+                          IntMap.insert x (tnode { _topLen = _topLen tnode - 1 }) $ -- TODO: lens for this
+                          IntMap.insert u (unode { _dlink = d }) $
+                          IntMap.insert d (dnode { _ulink = u }) ns
+
+
+-- This is (14) from [Knuth], the inverse operation to (12)
+uncover :: NodeIndex -> DLTable -> DLTable
+uncover i (DLTable ns ss nodes) = DLTable ns ss nodes'
+  where
+    nodes' = go (node i ^. ulink) nodes
+    node n = nodes IntMap.! n
+
+    go p ns | p == i    = let l = node i ^. llink
+                              r = node i ^. rlink
+                          in  relink l r ns
+            | otherwise = go (node p ^. ulink) (unhide p ns)
+
+    -- Relink the top node by rewiring the nodes on its left/right back to it
+    relink l r ns = let lnode = ns IntMap.! l
+                        rnode = ns IntMap.! r
+                    in
+                        IntMap.insert l (lnode { _rlink = i }) $
+                        IntMap.insert r (rnode { _llink = i }) ns
+
+
+-- This is (15) from [Knuth], the inverse operation to (13)
+unhide :: NodeIndex -> IntMap.IntMap Node -> IntMap.IntMap Node
+unhide p nodes = nodes'
+  where
+    nodes' = go (p-1) nodes
+    node n = nodes IntMap.! n
+
+    go q ns | q == p    = ns
+            | otherwise = let x = node q ^. topLen
+                              u = node q ^. ulink
+                              d = node q ^. dlink
+                          in
+                              -- Is q a spacer node
+                              if x <= 0
+                              then go d     ns
+                              else go (q-1) (relink x u d q ns)
+
+    -- Relink a node by rewiring the nodes above/below it back to it
+    relink x u d q ns = let unode = ns IntMap.! u
+                            dnode = ns IntMap.! d
+                            tnode = ns IntMap.! x
+                        in
+                            IntMap.insert x (tnode { _topLen = _topLen tnode + 1 }) $ -- TODO: lens for this
+                            IntMap.insert u (unode { _dlink = q }) $
+                            IntMap.insert d (dnode { _ulink = q }) ns
 
