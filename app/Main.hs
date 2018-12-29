6fa19bb6b6b304dd67468eccd4aba148753e8519
jasonincanada <nirgle@gmail.com>
2018-12-28 21:15:07 -0500

    formatting

--- app/Main.hs
+++ app/Main.hs
@@ -12,11 +12,10 @@ langfordInput :: Int -> ([Item], [Option])
 langfordInput n = (items, options)
   where
     items   = map show [1..n] ++ [ 's' : show i | i <- [1 .. 2*n] ]
-    options = [ [show i, 's':show j, 's':show k]
-                  | i <- [1..n],
-                    j <- [1..2*n-1],
-                    let k = i+j+1,
-                    k <= 2*n ]
+    options = [[show i, 's':show j, 's':show k] | i <- [1..n],
+                                                  j <- [1..2*n-1],
+                                                  let k = i+j+1,
+                                                  k <= 2*n ]
 
 -- I ran the following command once in the repl to create inputs/langford-pairs-3.links
 --
@@ -24,8 +23,7 @@ langfordInput n = (items, options)
 writeLinksFile :: String -> ([Item], [Option]) -> IO ()
 writeLinksFile name (items, options) = writeFile path string
   where
-    path = "inputs/" ++ name ++ ".links"
-
+    path   = "inputs/" ++ name ++ ".links"
     string = unlines [ unwords items,
                        "",
                        intercalate "\n" $ map unwords options ]
