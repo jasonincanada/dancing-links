d0f162e7a7148cff22d61a18d42e0ca9138efbef
jasonincanada <nirgle@gmail.com>
2018-12-31 01:19:12 -0500

    sudoku

--- app/Main.hs
+++ app/Main.hs
@@ -1,55 +1,28 @@
 module Main where
 
-import Data.List (intercalate)
+import Data.List (nub, sort)
 import DancingLinks
 import NanoParsec
+import Sudoku
 
 --file = "inputs/table10.links"
-file = "inputs/langford-pairs-3.links"
+--file = "inputs/langford-pairs-3.links"
 
--- Let's do the Langford pairs problem for n = 3 (p. 6)
-langfordInput :: Int -> ([Item], [Option])
-langfordInput n = (items, options)
+sudokuInput :: SudokuPuzzle -> ([Item], [Option])
+sudokuInput puzzle = (items options, options)
   where
-    items   = map show [1..n] ++ [ 's' : show i | i <- [1 .. 2*n] ]
-    options = [[show i, 's':show j, 's':show k] | i <- [1..n],
-                                                  j <- [1..2*n-1],
-                                                  let k = i+j+1,
-                                                  k <= 2*n ]
-
--- I ran the following command once in the repl to create inputs/langford-pairs-3.links
---
--- λ> writeLinksFile "langford-pairs-3" (langfordInput 3)
-writeLinksFile :: String -> ([Item], [Option]) -> IO ()
-writeLinksFile name (items, options) = writeFile path string
-  where
-    path   = "inputs/" ++ name ++ ".links"
-    string = unlines [ unwords items,
-                       "",
-                       intercalate "\n" $ map unwords options ]
+    items   = sort . nub . concat
+    options = sudokuOptions puzzle
 
 main :: IO ()
 main = do
-  contents <- readFile file
-  let links = run parseLinksFile contents
-  let table = tableFromLinks links
+  puzzle <- sudokuFromFile "inputs/newspaper.sudoku"
+  let input = sudokuInput puzzle
+  let table = tableFromLinks input
 
-  -- cover(i) and uncover(i) are inverse operations, this should be True
-  print $ table == uncover 1 (cover 1 table)
+  -- uncover(i) is the inverse of cover(i), this should be True
+  print $ (uncover 1 . cover 1) table == table
 
-  -- Run Algorithm D on the table and output the final state
-  print $ algorithmD table
+  -- Run Algorithm D on the table and output the solutions
+  print $ map sort $ _solutions (algorithmD table)
 
-  -- ... _solutions = [[["3","s2","s6"],["1","s3","s5"],["2","s1","s4"]],[["3","s1","s5"],["2","s3","s6"],["1","s2","s4"]]]}
-  --
-  -- There are two solutions to the Langford pairs problem for n = 3:
-  --
-  -- 1 s3 s5
-  -- 2 s1 s4
-  -- 3 s2 s6
-  --
-  -- and
-  --
-  -- 1 s2 s4
-  -- 2 s3 s6
-  -- 3 s1 s5
---
+++ src/Sudoku.hs
@@ -0,0 +1,61 @@
+module Sudoku
+  ( SudokuPuzzle(..)
+  , sudokuFromFile
+  , sudokuOptions
+  ) where
+
+import Control.Applicative (many)
+import Control.Category    ((>>>))
+import Data.List           (nub, (\\))
+import NanoParsec hiding   (item)
+
+type Row   = Int
+type Col   = Int
+type Digit = Int
+
+type SudokuPuzzle = [(Row, Col, Digit)]
+
+-- 1,7 2
+puzzleLine :: Parser (Row, Col, Digit)
+puzzleLine = (,,) <$> number <* comma
+                  <*> number <* space
+                  <*> number <* nl
+
+comma = char ','
+nl    = char '\n'
+
+sudokuFile :: Parser SudokuPuzzle
+sudokuFile = many puzzleLine
+
+sudokuFromFile :: FilePath -> IO SudokuPuzzle
+sudokuFromFile file = readFile file >>= (run sudokuFile >>> return)
+
+-- Get the options needed for the exact cover algorithm for a partial sudoku puzzle
+sudokuOptions :: SudokuPuzzle -> [[String]]
+sudokuOptions puzzle = unique (all \\ concatMap ruledOut puzzle)
+  where
+    all   = [[ item 'p' i j, item 'r' i k, item 'c' j k, item 'b' (box i j) k ]
+               | i <- [0..8],
+                 j <- [0..8],
+                 k <- [1..9]]
+
+    -- Which options are ruled out when we know a digit in a cell
+    ruledOut :: (Row, Col, Digit) -> [[String]]
+    ruledOut (row, col, digit) = cell ++ across ++ down ++ inbox
+      where
+        cell   = [[ item 'p' row col, item 'r' row k    , item 'c' col k    , item 'b' (box row col) k   ] | k <- [1..9] ]
+        across = [[ item 'p' row j  , item 'r' row digit, item 'c' j   digit, item 'b' (box row j) digit ] | j <- [0..8] ]
+        down   = [[ item 'p' i   col, item 'r' i   digit, item 'c' col digit, item 'b' (box i col) digit ] | i <- [0..8] ]
+        inbox  = [[ item 'p' i   j  , item 'r' i   digit, item 'c' j   digit, item 'b' (box i j)   digit ]
+                    | i <- [0..8],
+                      j <- [0..8],
+                      box i j == box row col ]
+
+    -- The 9x9 box number for a coordinate
+    box :: Row -> Col -> Int
+    box row col = 3*(row `div` 3) + (col `div` 3)
+
+    item :: Char -> Int -> Int -> String
+    item c m n = c : show m ++ show n
+
+    unique = Data.List.nub
